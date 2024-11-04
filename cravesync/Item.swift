ba7d4ade3c6c92ea9d00411813a4d0103a7975ad
Taylor Harwerth <tharwerth@gmail.com>
2024-11-04 17:10:02 -0600

    Initial Commit

---
+++ cravesync/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  cravesync
+//
+//  Created by Taylor Harwerth on 11/4/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ cravesyncTests/cravesyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  cravesyncTests.swift
+//  cravesyncTests
+//
+//  Created by Taylor Harwerth on 11/4/24.
+//
+
+import Testing
+@testable import cravesync
+
+struct cravesyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
